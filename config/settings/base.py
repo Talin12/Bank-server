e6c411f4453bd172f77cb675dfdc786573a87365
Talin Daga <talindaga692@gmail.com>
2025-12-27 20:40:20 +0530

    Setup api,postgresand mailpit containers

--- config/settings/base.py
+++ config/settings/base.py
@@ -135,7 +135,7 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROUTE = str(BASE_DIR / 'staticfiles')
+STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
