9b089dc39767189795d65bf5d2cd3beddc07b9f8
Talin Daga <talindaga692@gmail.com>
2026-01-02 15:30:42 +0530

    Feat: add custom auth with djoser and simplejwt

---
+++ core_apps/user_auth/serializers.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from djoser.serializers import (
+    UserCreateSerializer as DjoserUserCreateSerializer
+)
+
+User = get_user_model()
+
+class UserCreateSerializer(DjoserUserCreateSerializer):
+    class Meta(DjoserUserCreateSerializer.Meta):
+        model = User
+        fields = (
+            "email",
+            "username",
+            "password",
+            "first_name",
+            "last_name",
+            "id_no",
+            "securit_question",
+            "security_answer",
+        )
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
