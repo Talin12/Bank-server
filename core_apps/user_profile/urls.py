4a5d93303180f1484cc2d268b8917cb2048be2d8
Talin Daga <talindaga692@gmail.com>
2026-01-04 01:17:59 +0530

    feat: add profile serializers, tasks and views

---
+++ core_apps/user_profile/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import NextOfKinAPIView, NextOfKinDetailAPIView, ProfileListAPIView, ProfileDetailAPIView
+
+urlpatterns = [
+    path("all/", ProfileListAPIView.as_view(), name="all_profiles"),
+    path("my-profile/", ProfileDetailAPIView.as_view(), name="profile_detail"),
+    path("my-profile/next-of-kin/", NextOfKinAPIView.as_view(), name="next-of-kin-list"),
+    path("my-profile/next-of-kin/<uuid:pk>/", NextOfKinDetailAPIView.as_view(), name="next-of-kin-detail"),
+]
