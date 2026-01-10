324fc752a22ab381fec2bd53f3b9b721c95e37b3
Talin Daga <talindaga692@gmail.com>
2026-01-10 17:33:49 +0530

    feat: user can create,topup and delete virtual cards

--- config/settings/base.py
+++ config/settings/base.py
@@ -44,7 +44,8 @@ LOCAL_APPS = [
     "core_apps.common",
     "core_apps.user_auth",
     "core_apps.user_profile",
-    "core_apps.accounts"
+    "core_apps.accounts",
+    "core_apps.cards",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
