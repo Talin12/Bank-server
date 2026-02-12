262d1b6de3c668af69a0bca7a96100a94e2f4268
Talin Daga <talindaga692@gmail.com>
2026-02-13 00:01:07 +0530

    Fix views for user_auth

--- core_apps/user_auth/views.py
+++ core_apps/user_auth/views.py
@@ -62,7 +62,6 @@ class CustomTokenCreateView(TokenCreateView):
 
         logger.info(f"OTP sent for login to user: {user.email}")
 
-
         return Response(
             {"success": "OTP sent to your email", "email": user.email},
             status=status.HTTP_200_OK,
@@ -138,7 +137,52 @@ class CustomTokenRefreshView(TokenRefreshView):
         return refresh_res
 
 
- 
+class OTPVerifyView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        otp = request.data.get("otp")
+
+        if not otp:
+            return Response(
+                {"error": "OTP is required"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        user = User.objects.filter(otp=otp, otp_expiry_time__gt=timezone.now()).first()
+
+        if not user:
+            return Response(
+                {"error": "Invalid or expired OTP"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        if user.is_locked_out:
+            return Response(
+                {
+                    "error": f"Account is locked due to multiple failed login attempts. "
+                    f"Please try again after "
+                    f"{settings.LOCKOUT_DURATION.total_seconds() / 60} minutes "
+                },
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        user.verify_otp(otp)
+
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+        refresh_token = str(refresh)
+
+        response = Response(
+            {
+                "success": "Login successful. Now add your profile information, "
+                "so that we can create an account for you"
+            },
+            status=status.HTTP_200_OK,
+        )
+        set_auth_cookies(response, access_token, refresh_token)
+        logger.info(f"Successful login with OTP: {user.email}")
+        return response
+
 
 class LogoutAPIView(APIView):
     def post(self, request, *args, **kwargs):
